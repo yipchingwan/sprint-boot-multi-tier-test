7790feec4339ca13552542d56f35360f3fa1f15c
Liu Xia <lxconan@gmail.com>
2018-11-30 01:38:50 +0800

    [liuxia] feat: add flyway migration script on prod side.

---
+++ src/main/resources/db/migration/V1__create_parking_boy_table.sql
@@ -0,0 +1,5 @@
+create table parking_boy (
+    id              bigint          not null,
+    employee_id     varchar(64)     not null,
+    primary key (id)
+)
---
+++ src/main/resources/db/migration/V2__create_parking_lot_table.sql
@@ -0,0 +1,5 @@
+create table parking_lot (
+    id                  bigint  not null,
+    parking_boy_id      bigint  null,
+    primary key (id)
+)
