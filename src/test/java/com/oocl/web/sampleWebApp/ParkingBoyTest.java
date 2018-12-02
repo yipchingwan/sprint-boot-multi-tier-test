9363959877646e267a4f6c857029984ff2f5fd24
KYLE\Surface <kyle.public10415@gmail.com>
2018-12-02 19:19:21 +0800

    test: should_return_405_when_created_NULL_parking_boy

--- src/test/java/com/oocl/web/sampleWebApp/ParkingBoyTest.java
+++ src/test/java/com/oocl/web/sampleWebApp/ParkingBoyTest.java
@@ -38,4 +38,17 @@ public class ParkingBoyTest {
         //Then
         assertEquals(201, result.getResponse().getStatus());
     }
+
+    @Test
+    public void should_return_405_when_created_NULL_parking_boy() throws Exception {
+        //Given
+        final ParkingBoy boy = parkingBoyRepository.save(new ParkingBoy("boy"));
+        final ObjectMapper objectMapper = new ObjectMapper();
+        //When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/parkingboys").
+                contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(null))).andReturn();
+
+        //Then
+        assertEquals(405, result.getResponse().getStatus());
+    }
 }
